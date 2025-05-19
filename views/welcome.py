c862eeb796ad3c82fc3058d31e6afd69658531ed
davitdavtyan969 <david.davtyan96@gmail.com>
2025-05-19 22:12:05 +0400

    Added new welcome page which appears after the user gets sign in

---
+++ views/welcome.py
@@ -0,0 +1,13 @@
+# views/welcome.py
+import streamlit as st
+
+def welcome_page():
+    st.title("🎉 You're in — Welcome aboard!")
+    st.write("""
+        Now let’s build your personalized semester planner together. 🚀
+        We’ll guide you through a few quick and easy steps — it’ll take less than a minute, 
+        and by the end, you’ll have a custom course plan tailored just for you.
+        Ready to make your next semester your best one yet? Let’s go! 👇
+    """)
+    if st.button("Next"):
+        st.session_state.page = "transcript_intro"
